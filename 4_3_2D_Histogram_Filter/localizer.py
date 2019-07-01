a1e9cf7706e3a099c9ec91af12c7df862c9cd9f6
Xuemeng Zhang <nxzhangxm@gmail.com>
2019-07-01 04:41:18 -0700

    complete 2d histogram filter

--- 4_3_2D_Histogram_Filter/localizer.py
+++ 4_3_2D_Histogram_Filter/localizer.py
@@ -20,6 +20,23 @@ def sense(color, grid, beliefs, p_hit, p_miss):
     #
     # TODO - implement this in part 2
     #
+    height = len(grid)
+    width = len(grid[0])
+
+    total = 0
+    for r in range(height):
+        row = []
+        for c in range(width):
+            is_hit = (color == grid[r][c])
+            new_prob = beliefs[r][c] * (is_hit * p_hit + (1 - is_hit)
+                                        * p_miss)
+            row.append(new_prob)
+            total += new_prob
+        new_beliefs.append(row)
+
+    for r in range(height):
+        for c in range(width):
+            new_beliefs[r][c] /= total
 
     return new_beliefs
 
@@ -29,8 +46,8 @@ def move(dy, dx, beliefs, blurring):
     new_G = [[0.0 for i in range(width)] for j in range(height)]
     for i, row in enumerate(beliefs):
         for j, cell in enumerate(row):
-            new_i = (i + dy ) % width
-            new_j = (j + dx ) % height
+            new_i = (i + dy ) % height
+            new_j = (j + dx ) % width
             # pdb.set_trace()
             new_G[int(new_i)][int(new_j)] = cell
     return blur(new_G, blurring)
